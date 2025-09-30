fb0a4092f9b9ba8c4a10f9b6779391dbcdfed275
Bertram <bertram.englert@gmx.at>
2025-09-30 23:40:02 +0200

    Initial commit

---
+++ src/main/java/com/github/thefreshprincefrombelair/dedalus/DedalusApplication.java
@@ -0,0 +1,13 @@
+package com.github.thefreshprincefrombelair.dedalus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedalusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DedalusApplication.class, args);
+    }
+
+}
